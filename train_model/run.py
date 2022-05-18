b06f8c9d63e0e2c2ec2b6bca1b3c03bf2f371cf7
lilianabs <lilianabsmath@gmail.com>
2022-05-18 18:13:16 -0500

    Import ml package

--- train_model/run.py
+++ train_model/run.py
@@ -1,10 +1,24 @@
 # Script to train machine learning model.
 
+import logging
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml import process_data
+
+# Configure logging
+logging.basicConfig(
+    filename='../logs/ml-pipeline.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
+
 
 # Add code to load in the data.
+data = pd.read_csv("../data/clean.csv")
+logging.info("Load clean data")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
