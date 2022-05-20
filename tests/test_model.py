3c44bdc4b6e431a6ac5969efeefe539e9eb9a5d8
lilianabs <lilianabsmath@gmail.com>
2022-05-19 19:23:40 -0500

    Add tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,69 @@
+import joblib
+import pandas as pd
+import numpy as np
 import pytest
 import sys
 
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+
 sys.path.insert(0, "../")
 
+from train_model.ml.data import process_data
 from train_model.ml.model import train_model
+from train_model.ml.model import inference
 
-def test_train_model():
-    assert True == True
+CAT_FEATURES = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+    ]
+
+@pytest.fixture
+def data():
+    return pd.read_csv("data/clean.csv")
+
+def test_process_data(data):
+
+    X_train, y_train, encoder, lb = process_data(
+        data, categorical_features=CAT_FEATURES, label="salary", training=True
+    )
+    
+    assert X_train.shape[0] > 0
+    assert X_train.shape[1] > 0
+    assert isinstance(y_train, np.ndarray) 
+    assert isinstance(encoder, OneHotEncoder)
+    assert isinstance(lb, LabelBinarizer)
+
+def test_train_model(data):
+
+    X_train, y_train, encoder, lb = process_data(
+        data, categorical_features=CAT_FEATURES, label="salary", training=True
+    )
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, RandomForestClassifier)
+
+
+def test_inference(data):
+
+    X_train, y_train, encoder, lb = process_data(
+        data, categorical_features=CAT_FEATURES, label="salary", training=True
+    )
+
+    X_test, y_test, encoder, lb = process_data(
+        data, categorical_features=CAT_FEATURES, label="salary", training=False,
+        encoder=encoder, lb=lb
+    )
+
+    model = joblib.load("model/model.pkl")
+    
+    preds = inference(model, X_train)
+
+    assert isinstance(preds, np.ndarray)
